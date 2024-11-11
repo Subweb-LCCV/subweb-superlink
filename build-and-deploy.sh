5cc9c8e863b04007ec12f4052a5eb98dc81bbe07
Diogo Cintra <diogotc@lccv.ufal.br>
2024-11-11 10:33:18 -0300

    Add Dockerfile and build script

---
+++ build-and-deploy.sh
@@ -0,0 +1,2 @@
+docker build -t southamerica-east1-docker.pkg.dev/subweb-superlink/subweb-superlink/web:latest .
+docker push southamerica-east1-docker.pkg.dev/subweb-superlink/subweb-superlink/web:latest
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { PortalAuthContext } from "./components/PortalAuth";
-import clipboard from "./assets/copy-to-clipboard.svg";
 import "./App.css";
 
 const App = () => {
   const { token, urlPortal } = useContext(PortalAuthContext);
 
-  const handleClick = () => {
-    navigator.clipboard
-      .writeText(token)
-      .then(() => {
-        console.log("Token copied to clipboard!");
-      })
-      .catch((err) => {
-        console.error("Failed to copy text: ", err);
-      });
-  };
-
   return (
     <>
       <div className="card">
         <p>Your personal user token:</p>
-        <input type="text" value={token} readOnly />
-        <div onClick={() => handleClick()}>
-          <img
-            src={clipboard}
-            alt="Copy to clipboard"
-            height="20px"
-            className="invert"
-          />
-        </div>
+        <input type="text" value={token} />
       </div>
       <p className="read-the-docs">Portal: {urlPortal}</p>
     </>
